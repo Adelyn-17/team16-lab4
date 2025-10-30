0c3f14ec5cd88a1fe9c1dd9aae4ad441aa03a53c
Adelyn-17 <2837919089@qq.com>
2025-10-30 15:39:25 +0800

    Update pll.v

--- pll.v
+++ pll.v
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ns
 module pll(
-    input wire sys_clk , //System Clock, 50MHz
-    input wire sys_rst_n , //Reset signal. Low level is effective
+    input wire sys_clk , 
+    input wire sys_rst_n , 
 
-    output wire vga_clk // VGA Clock, 25MHz
+    output wire vga_clk 
 );
 
 reg clk_25;
@@ -16,5 +16,6 @@ always @(posedge sys_clk or negedge sys_rst_n) begin
 end
 
 assign vga_clk = clk_25;
+
 
 endmodule
